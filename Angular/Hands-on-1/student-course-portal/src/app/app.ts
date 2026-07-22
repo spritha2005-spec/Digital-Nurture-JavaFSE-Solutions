f02a246e77b5f3a052895f87fb2164aca963b8d1
Pritha Saha <spritha2005@gmail.com>
2026-07-22 13:03:18 +0530

    Added Angular Hands-on 1

---
+++ Angular/Hands-on-1/student-course-portal/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { Header } from './components/header/header';
+import { Home } from './pages/home/home';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Header, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'student-course-portal';
+}
